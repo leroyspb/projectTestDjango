c6dd1ad8bd6b6390015d7e0bca62836b8b6a6517
leroyspb <leroy777@bk.ru>
2024-03-24 21:47:55 +0300

    24.03

--- project/venv/Scripts/activate.bat
+++ project/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\leroy\PycharmProjects\projectTestDjango\venv
+set VIRTUAL_ENV=D:\Documents\git_test_clone\projectTestDjango\project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
